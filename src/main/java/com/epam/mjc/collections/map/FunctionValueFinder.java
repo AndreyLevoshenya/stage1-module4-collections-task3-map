234b7a5142186b3bbcb571cba65d443135a3e124
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-12 16:44:20 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        return functionMap.containsKey(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer item : sourceList) {
+            map.put(5 * item + 2, item);
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("[,. ]");
+        for (String word : words) {
+            if(!map.isEmpty()) {
+                if(!map.containsKey(word) && !word.isEmpty()) {
+                    map.put(word, 1);
+                }
+                else {
+                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                        if(entry.getKey().equals(word)) {
+                            entry.setValue(entry.getValue() + 1);
+                        }
+                    }
+                }
+            }
+            else if(!word.isEmpty()){
+                map.put(word, 1);
+            }
+        }
+        return map;
     }
 }
