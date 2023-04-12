b9c6a9bd856345f65279adc122c7bee9c16f30a6
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-12 20:14:17 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -8,20 +8,11 @@ public class WordRepetitionMapCreator {
         Map<String, Integer> map = new HashMap<>();
         String[] words = sentence.toLowerCase().split("[,. ]");
         for (String word : words) {
-            if(!map.isEmpty()) {
-                if(!map.containsKey(word) && !word.isEmpty()) {
-                    map.put(word, 1);
-                }
-                else {
-                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                        if(entry.getKey().equals(word)) {
-                            entry.setValue(entry.getValue() + 1);
-                        }
-                    }
-                }
-            }
-            else if(!word.isEmpty()){
+            if(!word.isEmpty() && !map.containsKey(word)) {
                 map.put(word, 1);
+            }
+            else if(map.containsKey(word)){
+                map.replace(word, map.get(word) + 1);
             }
         }
         return map;
